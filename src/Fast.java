3f457d788858404fbe4398483dca3d3dc15e6210
Gennadii <gorelikov.gennadii@gmail.com>
2014-10-11 01:41:34 +0400

    hope it pass test

--- src/Fast.java
+++ src/Fast.java
@@ -19,40 +19,50 @@ public class Fast {
             a[i] = new Point(x, y);
             a[i].draw();
         }       
+        boolean ignored = false;
         for (int i = 0; i < N - 1; i++)
         {
-            Arrays.sort(a, i, N);
-            Arrays.sort(a, i, N, a[i].SLOPE_ORDER);
-            lineSlope = a[i].slopeTo(a[i+1]);
+            Arrays.sort(a);
+            Arrays.sort(a, a[i].SLOPE_ORDER);
+            lineSlope = a[0].slopeTo(a[1]);
             lineSeqCount = 2;
-            for (int j = i + 2; j < N; j++)
+            if (a[0].compareTo(a[1]) > 0)
+                ignored = true;
+            else
+                ignored = false;
+            for (int j = 2; j < N; j++)
             {
-                if (a[i].slopeTo(a[j]) == lineSlope)
+                if (a[0].slopeTo(a[j]) == lineSlope)
                 {
                     lineSeqCount += 1;
                 }
                 else
                 {
-                    lineSlope = a[i].slopeTo(a[j]);
-                    if (lineSeqCount >= 4)
+                    lineSlope = a[0].slopeTo(a[j]);
+                    if (lineSeqCount >= 4 && !ignored)
                     {
-                        processLinePoints(a, j + 1 - lineSeqCount, j - 1, i);
+                        processLinePoints(a, j + 1 - lineSeqCount, j - 1);
                     }
+                    ignored = false;
                     lineSeqCount = 2;
                 }
+                if (a[0].compareTo(a[j]) > 0)
+                {
+                    ignored = true;
+                }
             }
-            if (lineSeqCount >= 4)
+            if (lineSeqCount >= 4 && !ignored)
             {
-                processLinePoints(a, N + 1 - lineSeqCount, N-1, i);
+                processLinePoints(a, N + 1 - lineSeqCount, N-1);
             }
         }
     }
     
-    private static void processLinePoints(Point[] a, int first, int last, int pivot)
+    private static void processLinePoints(Point[] a, int first, int last)
     {
         Arrays.sort(a, first, last + 1);
-        a[pivot].drawTo(a[last]);
-        StdOut.print(a[pivot].toString() + " -> ");
+        a[0].drawTo(a[last]);
+        StdOut.print(a[0].toString() + " -> ");
         for (int i = first; i < last; i++)
         {
             StdOut.print(a[i].toString() + " -> ");
